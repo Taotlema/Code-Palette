a8b1539a5b2be936d1b4d78c6f8142d7dd1a93d0
Ayemhenre Isikhuemhen <127813264+Taotlema@users.noreply.github.com>
2025-02-07 10:00:59 -0500

    Update insertion_sort.py

--- Python Code/python_algorithms/insertion_sort.py
+++ Python Code/python_algorithms/insertion_sort.py
@@ -25,4 +25,24 @@ def insertionSort(arr):
             j -= 1
         arr[j + 1] = key
 
-        
+# Testing the code above
+
+def printArray(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+if __name__=="__main__":
+    arr_1 = [12, 11, 13, 5, 6]
+    arr_2 = [3, 4, 9, 2, 10, 12]
+    arr_3 = [5, 4, 3, 2, 1]
+
+    insertionSort(arr_1)
+    insertionSort(arr_2)
+    insertionSort(arr_3)
+
+    printArray(arr_1)
+    printArray(arr_2)
+    printArray(arr_3)
+
+    
